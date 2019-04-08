fe17c686eeec6713706d16c7899e9c874f1adbbc
Artem Knyazev <artem.e.knyazev@yandex.ru>
2019-04-08 19:17:45 +0300

    Try publish to npm

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -2,7 +2,3 @@
 set -euf -o pipefail
 npm run cleanup
 npm run build
-rm $(pwd)/package-lock.json || true
-rm $(pwd)/npm-shrinkwrap.json || true
-NODE_ENV=production npm install --production
-npm shrinkwrap
